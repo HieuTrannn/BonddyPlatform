1dd8aea0adef9ee5325bd936fbf4afe136887753
HieuTrannn <hieutran4525@gmail.com>
2026-01-17 17:43:07 +0700

    fix pro

--- BonddyPlatform.API/Program.cs
+++ BonddyPlatform.API/Program.cs
@@ -20,8 +20,11 @@ if (!string.IsNullOrWhiteSpace(databaseUrl))
     var uri = new Uri(databaseUrl);
     var userInfo = uri.UserInfo.Split(':');
 
-    conn = $"Host={uri.Host};Port={uri.Port};Database={uri.AbsolutePath.TrimStart('/')};" +
+    var port = uri.IsDefaultPort || uri.Port <= 0 ? 5432 : uri.Port;
+
+    conn = $"Host={uri.Host};Port={port};Database={uri.AbsolutePath.TrimStart('/')};" +
            $"Username={userInfo[0]};Password={userInfo[1]};Ssl Mode=Require;Trust Server Certificate=true";
+
 }
 
 builder.Services.AddDbContext<BonddyDbContext>(opt => opt.UseNpgsql(conn));
